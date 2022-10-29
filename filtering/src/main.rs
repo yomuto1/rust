1675c8a02b82858c6016d42196a6b1c6aac870c0
yomuto <yomuto@gmail.com>
2022-10-29 22:58:17 +0900

    bug fix in kernel size

--- filtering/src/main.rs
+++ filtering/src/main.rs
@@ -5,16 +5,17 @@ use std::io::Write;
 use std::time::{Instant};
 
 const ITERATION: usize = 1000;
-const INP_WID: usize = 1280;
-const INP_HEI: usize = 720;
-const OUT_WID: usize = 640;
-const OUT_HEI: usize = 360;
+const INP_WID: usize = 512;
+const INP_HEI: usize = 512;
+const OUT_WID: usize = 256;
+const OUT_HEI: usize = 256;
+const VAL_SHIFT: i32 = 4;
 
 fn main() {
-	let inp_filename = r"E:\ICSP\TestSequences\1280x720\bigships_1280x720.yuv";
-	let out_filename = r"bigships_1280x720.y";
-	let out_filtered_filename = r"bigships_1280x720_filtered.y";
-	let out_pool_filename = r"bigships_640x360_pool.y";
+	let inp_filename = r"/home/hyuk/code/barbara_gray.raw";
+	let out_filename = r"barbara_512x512.y";
+	let out_filtered_filename = r"barbara_512x512_filtered.y";
+	let out_pool_filename = r"barbara_256x256_pool.y";
 	
     let mut inp_file = File::open(&inp_filename).expect("no inp file found");
     //let metadata = fs::metadata(&inp_filename).expect("unable to read metadata");
@@ -45,17 +46,28 @@ fn main() {
         count += 1;
 
 		// filtering
-		for j in 0..(INP_HEI - 1) {
-			for i in 0..(INP_WID - 1) {
+		for j in 0..(INP_HEI - 2) {
+			for i in 0..(INP_WID - 2) {
 				let mut sum_s32: i32 = 0;
 				
-				for k_h in 0..2 {
-					for k_w in 0..2 {
-						sum_s32 += (a_out_y_u8[k_w + k_h * INP_WID + i + j * INP_WID] as i16 * a_coefficient_s16[k_w + k_h * 3]) as i32;
+				for k_h in 0..3 {
+					for k_w in 0..3 {
+						let ker_s16: i16 = a_coefficient_s16[k_w + k_h * 3];
+						let inp_u8: u8 = a_out_y_u8[k_w + i + (k_h + j) * INP_WID];
+
+						sum_s32 += (inp_u8 as i16 * ker_s16) as i32;
+
+						//if i == 0 && j == 0 {
+						//	println!("{k_w}, {k_h}: {ker_s16}, {inp_u8}, {sum_s32}");
+						//}
 					}
 				}
+
+				//if i == 0 && j == 0 {
+				//	println!("{i}, {j}: {sum_s32}");
+				//}
 				
-				sum_s32 >>= 3;
+				sum_s32 >>= VAL_SHIFT;
 				if sum_s32 > 255 {
 					sum_s32 = 255;
 				}
@@ -63,13 +75,17 @@ fn main() {
 					sum_s32 = 0;
 				}
 				
+				//if i == 0 && j == 0 {
+				//	println!("{sum_s32}");
+				//}
+				
 				a_out_filtered_y_u8[i + j * INP_WID] = sum_s32 as u8;
 			}
 		}
 		
 		// max pooling
-		for j in 0..(OUT_HEI - 1) {
-			for i in 0..(OUT_WID - 1) {
+		for j in 0..OUT_HEI {
+			for i in 0..OUT_WID {
 				let pel_00_u8: u8 = a_out_filtered_y_u8[(i * 2 + 0) + (j * 2 + 0) * INP_WID];
 				let pel_10_u8: u8 = a_out_filtered_y_u8[(i * 2 + 1) + (j * 2 + 0) * INP_WID];
 				let pel_01_u8: u8 = a_out_filtered_y_u8[(i * 2 + 0) + (j * 2 + 1) * INP_WID];
